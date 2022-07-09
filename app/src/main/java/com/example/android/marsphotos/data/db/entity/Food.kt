8ad6257e00856be76e8ae42e82633b960587d278
Tùng Nguyễn Tiến <tientung1412@gmail.com>
2022-07-09 21:02:52 +0700

    fix name

---
+++ app/src/main/java/com/example/android/marsphotos/data/db/entity/Food.kt
@@ -0,0 +1,29 @@
+package com.example.android.marsphotos.data.db.entity
+
+import com.google.firebase.database.PropertyName
+import java.util.*
+
+data class Food(
+    val id: Int,
+    val foodName: String,
+    val price: Int,
+    val foodImg : FoodImg,
+    val category: Category,
+)
+
+data class FoodInfo(
+    @get:PropertyName("foodID") @set:PropertyName("foodID") var foodId: Int = 0,
+    @get:PropertyName("billingId") @set:PropertyName("billingId") var billingId: Int = 0,
+    @get:PropertyName("quantity") @set:PropertyName("quantity") var quantity: Int = 0,
+    @get:PropertyName("note") @set:PropertyName("note") var note: String? = "",
+    @get:PropertyName("updatedAt") @set:PropertyName("updatedAt") var updatedAt: Long = Date().time,
+    @get:PropertyName("isBring") @set:PropertyName("isBring") var isBring: Boolean? = false,
+)
+
+data class FoodItem(
+    var food: Food,
+    var billingId: Int,
+    var note: String?,
+    var quantity: Int,
+    var updatedAt: Long,
+)
--- app/src/main/java/com/example/android/marsphotos/data/db/entity/FoodBilling.kt
+++ app/src/main/java/com/example/android/marsphotos/data/db/entity/FoodBilling.kt
@@ -1,17 +1,7 @@
 package com.example.android.marsphotos.data.db.entity
 
 import com.google.firebase.database.PropertyName
-import java.util.*
 import kotlin.collections.ArrayList
-
-
-data class Food(
-    val id: Int,
-    val foodName: String,
-    val price: Int,
-    val foodImg : FoodImg,
-    val category: Category,
-)
 
 data class FoodBilling(
     @get:PropertyName("foodRequests") @set:PropertyName("foodRequests") var foodRequests: ArrayList<FoodInfo>? = arrayListOf(),
@@ -19,21 +9,4 @@ data class FoodBilling(
     @get:PropertyName("foodDones") @set:PropertyName("foodDones") var foodDones: ArrayList<FoodInfo>? = arrayListOf(),
     @get:PropertyName("foodCanceleds") @set:PropertyName("foodCanceleds") var foodCanceleds: ArrayList<FoodInfo>? = arrayListOf(),
     @get:PropertyName("note") @set:PropertyName("note") var note: String = "",
-)
-
-data class FoodInfo(
-    @get:PropertyName("foodID") @set:PropertyName("foodID") var foodId: Int = 0,
-    @get:PropertyName("billingId") @set:PropertyName("billingId") var billingId: Int = 0,
-    @get:PropertyName("quantity") @set:PropertyName("quantity") var quantity: Int = 0,
-    @get:PropertyName("note") @set:PropertyName("note") var note: String? = "",
-    @get:PropertyName("updatedAt") @set:PropertyName("updatedAt") var updatedAt: Long = Date().time,
-    @get:PropertyName("isBring") @set:PropertyName("isBring") var isBring: Boolean? = false,
-)
-
-data class FoodItem(
-    var food: Food,
-    var billingId: Int,
-    var note: String?,
-    var quantity: Int,
-    var updatedAt: Long,
 )
